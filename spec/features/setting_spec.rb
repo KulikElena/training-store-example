25c6d774221d4a448ce0f962ede8dc5a51fbed78
ElenaKulik <lena.kulik.4400@gmail.com>
2020-02-14 10:40:01 +0200

    Add specs for settings page

--- spec/features/setting_spec.rb
+++ spec/features/setting_spec.rb
@@ -3,15 +3,15 @@ RSpec.describe 'Settings' do
   # Create an instance of SettingPage class and assign it to setting_page variable
   let(:setting_page) { SettingPage.new }
   let(:user) { create(:user) }
-  let(:address) { create(:address) }
+  #let(:address) { create(:address) }
 
-  #let(:first_name) { FFaker::Name.first_name }
-  #let(:last_name) { FFaker::Name.last_name }
-  #let(:address) { FFaker::Address.street_address }
-  #let(:city) { FFaker::Address.city }
-  #let(:zip) { FFaker::AddressAU.postcode }
-  #let(:country) { %w[Ukraine Germane USA UK].sample }
-  #let(:phone) { FFaker::PhoneNumberAU.international_mobile_phone_number }
+  let(:first_name) { FFaker::Name.first_name }
+  let(:last_name) { FFaker::Name.last_name }
+  let(:address) { FFaker::Address.street_address }
+  let(:city) { FFaker::Address.city }
+  let(:zip) { FFaker::AddressAU.postcode }
+  let(:country) { %w[Ukraine Germane USA UK].sample }
+  let(:phone) { FFaker::PhoneNumberUA.international_mobile_phone_number.gsub!(/[\s-]/, '') }
 
   # Login user and oper Settings page
   before do
@@ -19,7 +19,7 @@ RSpec.describe 'Settings' do
     setting_page.load
   end
 
-  context 'check shipping and billing address with valid data' do
+  context 'check filling shipping and billing address with the valid data' do
     # Check that url http://localhost:2525/settings opened
     it { expect(setting_page).to be_displayed }
     # Check that all elements are displayed
@@ -27,7 +27,7 @@ RSpec.describe 'Settings' do
     # Check that title is correct
     it { expect(setting_page.title).to eq('Bookstore') }
 
-    it 'fill in all data to shipping address with correct data' do
+    it 'fill in all correct data to shipping address' do
      setting_page.shipping_address_first_name.set(first_name)
      setting_page.shipping_address_last_name.set(last_name)
      setting_page.shipping_address_address.set(address)
@@ -36,11 +36,9 @@ RSpec.describe 'Settings' do
      setting_page.shipping_address_country.select(country)
      setting_page.shipping_address_phone.set(phone)
      setting_page.shipping_address_button.click
-
-     #expect(setting_page).to have_content(cannot_be_blank)
     end
 
-    #it 'fill in all data to billing address with correct data' do
+    it 'fill in all correct data to billing address' do
      setting_page.billing_address_first_name.set(first_name)
      setting_page.billing_address_last_name.set(last_name)
      setting_page.billing_address_address.set(address)
@@ -52,13 +50,85 @@ RSpec.describe 'Settings' do
      end
   end
 
-  context 'check shipping and billing address with invvalid data' do
-    xit 'fill in all data to shipping address with incorrect data' do
+  context 'check shipping address with invvalid data' do
+    it 'fill in shipping first name more than 50 characters' do
       first_name = FFaker::Lorem.characters(rand(51..100))
-      #puts first_name
       setting_page.shipping_address_first_name.set(first_name)
       setting_page.shipping_address_button.click
-      #setting_page.shipping_address_first_name_validation.to eq("is too long (maximum is 50 characters)")
+      expect(setting_page.shipping_address_first_name_validation.text).to eq("is too long (maximum is 50 characters)")
     end
+
+    it 'empty first name in shipping' do
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_first_name_validation.text).to eq("can't be blank")
+    end
+
+    it 'fill in shipping last name more than 50 characters' do
+      last_name = FFaker::Lorem.characters(rand(51..100))
+      setting_page.shipping_address_last_name.set(last_name)
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_last_name_validation.text).to eq("is too long (maximum is 50 characters)")
+    end
+
+    it 'empty last name in shipping' do
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_last_name_validation.text).to eq("can't be blank")
+    end
+
+    it 'fill in shipping address more than 20 characters' do
+      address = FFaker::Lorem.characters(rand(21..50))
+      setting_page.shipping_address_address.set(address)
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_address_validation.text).to eq("is too long (maximum is 20 characters)")
+    end
+
+    it 'empty address in shipping' do
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_address_validation.text).to eq("can't be blank")
+    end
+
+    it 'fill in shipping city more than 50 characters' do
+      city = FFaker::Lorem.characters(rand(51..100))
+      setting_page.shipping_address_city.set(city)
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_city_validation.text).to eq("is too long (maximum is 50 characters)")
+    end
+
+    it 'empty city in shipping' do
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_city_validation.text).to eq("can't be blank")
+    end
+
+    it 'fill in zip more than 5 characters' do
+      zip = rand(1000000..10000000)
+      setting_page.shipping_address_zip.set(zip)
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_zip_validation.text).to eq("is too long (maximum is 5 characters)")
+    end
+
+    it 'empty zip in shipping' do
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_zip_validation.text).to eq("can't be blank")
+    end
+
+    it 'fill in zip with characters in shipping' do
+      zip = FFaker::Lorem.characters(rand(1..5))
+      setting_page.shipping_address_zip.set(zip)
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_zip_validation.text).to eq("is invalid")
+    end
+
+    it 'empty dropdown Country in shipping' do
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_country_validation.text).to eq("can't be blank")
+    end
+
+    it 'fill in phone with characters in shipping' do
+      phone = FFaker::Lorem.characters(rand(5..13))
+      setting_page.shipping_address_phone.set(phone)
+      setting_page.shipping_address_button.click
+      expect(setting_page.shipping_address_phone_validation.text).to eq("Phone must be format +38002353535")
+    end
+
   end
 end
